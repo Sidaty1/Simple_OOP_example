3d1347b823d71fab3b195b29b27dc3d730b22f20
Sidaty1 <Sidaty.elhadrami@yahoo.com>
2020-02-29 02:12:05 +0100

    Create classes

---
+++ game.py
@@ -0,0 +1,23 @@
+class Player:
+
+	def __init__(self, arg):
+		
+
+
+class EasyMonster:
+
+	def __init__(self, arg):
+		
+
+
+class DifficultMonster(EasyMonster) :
+
+	def __init__(self, arg):
+		
+
+class Dice:
+
+	pass
+
+		
+		
